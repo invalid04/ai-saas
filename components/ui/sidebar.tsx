d57b37821a63a2045f8222ca4b1063f45770d971
invalid04 <68178371+invalid04@users.noreply.github.com>
2023-07-21 05:38:37 -0500

    sidebar progress

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -1,6 +1,64 @@
 "use client"
 import Link from 'next/link'
 import Image from 'next/image'
+import { Montserrat } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { 
+    LayoutDashboard, 
+    MessageSquare, 
+    ImageIcon, 
+    VideoIcon, 
+    Music, Code, 
+    Settings } from 'lucide-react'
+
+const montserrat = Montserrat({
+    weight: '600',
+    subsets: ["latin"],
+})
+
+const routes = [
+    {
+      label: 'Dashboard',
+      icon: LayoutDashboard,
+      href: '/dashboard',
+      color: "text-sky-500"
+    },
+    {
+      label: 'Conversation',
+      icon: MessageSquare,
+      href: '/conversation',
+      color: "text-violet-500",
+    },
+    {
+      label: 'Image Generation',
+      icon: ImageIcon,
+      color: "text-pink-700",
+      href: '/image',
+    },
+    {
+      label: 'Video Generation',
+      icon: VideoIcon,
+      color: "text-orange-700",
+      href: '/video',
+    },
+    {
+      label: 'Music Generation',
+      icon: Music,
+      color: "text-emerald-500",
+      href: '/music',
+    },
+    {
+      label: 'Code Generation',
+      icon: Code,
+      color: "text-green-700",
+      href: '/code',
+    },
+    {
+      label: 'Settings',
+      icon: Settings,
+      href: '/settings',
+    },
+  ];
 
 const Sidebar = () => {
     return (
@@ -18,7 +76,23 @@ const Sidebar = () => {
                             src='/logo.png'
                          />
                     </div>
+                    <h1 className={cn('text-2xl font-bold', montserrat.className)}>Genius</h1>
                 </Link>
+                <div className='space-y-1'>
+                    {routes.map((route) => (
+                        <Link 
+                            href={route.href}
+                            key={route.href}
+                            className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer 
+                            hover:text-white hover:bg-white/10 rounded-lg transition'
+                        >
+                            <div className='flex items-center flex-1'>
+                                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                                {route.label}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     )
