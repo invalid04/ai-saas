2f9cc9032ca1bbc5e7d27c868f6e31908e5bf0a0
invalid04 <68178371+invalid04@users.noreply.github.com>
2023-07-21 06:58:13 -0500

    finished sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,14 @@
 import { UserButton } from "@clerk/nextjs"
-import { Button } from "./button"
-import { Menu } from 'lucide-react'
 import MobileSidebar from "./mobile-sidebar"
 
 const Navbar = () => {
     return (
-        <nav className='flex items-center p-4'>
-            <MobileSidebar />
+        <div className='flex items-center p-4'>
+            <MobileSidebar/>
             <div className='flex w-full justify-end'>
                 <UserButton afterSignOutUrl="/" />
             </div>
-        </nav>
+        </div>
     )
 }
 
